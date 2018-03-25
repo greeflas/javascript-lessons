10308fb6febf38c67420cc230e4102c719e2d306
greeflas <vldmr.kuprienko@gmail.com>
2018-03-25 13:27:06 +0300

    Data types

--- lesson1/js/scripts.js
+++ lesson1/js/scripts.js
@@ -7,3 +7,15 @@ var letter = 'r'; // char letter = 'r';
 var str = 'this is some string'; // char str[] = "this is some string";
 var arr = [3, 'g', false, 5.6, 'string'];
 var nothing = null; // NULL, nullpointer
+
+// data types
+
+console.log(typeof num);
+console.log(typeof sum);
+console.log(typeof flag);
+console.log(typeof letter);
+console.log(typeof str);
+console.log(typeof arr);
+console.log(typeof nothing);
+console.log(8 / 0);
+console.log('sting' - 6);
