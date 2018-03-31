86811e0f41e1d59ddf09b99d632bb5d97fb9a3eb
greeflas <vldmr.kuprienko@gmail.com>
2018-03-31 16:05:19 +0300

    if-else

---
+++ lesson2/js/scripts.js
@@ -0,0 +1,8 @@
+// if-else
+var username = prompt('Input your username', '');
+
+if (username == 'admin') {
+    alert('Welcome to the admin panel!');
+} else {
+    alert('Access to the admin panel is denied!');
+}
