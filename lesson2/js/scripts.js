9fa10f69040a4605c493e853c5e8eb8795b1cd72
greeflas <vldmr.kuprienko@gmail.com>
2018-03-31 16:05:33 +0300

    switch

--- lesson2/js/scripts.js
+++ lesson2/js/scripts.js
@@ -6,3 +6,26 @@ if (username == 'admin') {
 } else {
     alert('Access to the admin panel is denied!');
 }
+
+// switch
+var a = prompt('Input a', 0);
+var op = prompt('Input operation', '+');
+var b = prompt('Input b', 0);
+var result;
+
+switch (op) {
+    case '+':
+        result = a + b;
+        break;
+    case '-':
+        result = a - b;
+        break;
+    case '*':
+        result = a * b;
+        break;
+    case '/':
+        result = a / b;
+        break;
+}
+
+alert('Result: ' + result);
